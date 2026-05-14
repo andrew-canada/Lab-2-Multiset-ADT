c70e24fd89691b55717f21e779c3348948b80560
mdmclbkairmnas <ia.wang@mail.utoronto.ca>
2026-05-14 19:49:40 -0400

    Edited Tree.java

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,59 @@
+import java.util.ArrayList;
+
 public class Tree {
-    // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    private Integer root;
+    private ArrayList<Tree> subtrees;
+    private int size;
+
+    public Tree(Integer root) {
+        this.root = root;
+        this.subtrees = new ArrayList<Tree>();
+    }
+
+    public Tree(Integer root, ArrayList<Tree> subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    public int length() {
+        if(this.isEmpty()) {
+            return 0;
+        } else {
+            int size = 1;
+            for(Tree tree: this.subtrees) {
+                size += tree.length();
+            }
+            return size;
+        }
+    }
+
+    public int count(int item) {
+        if(this.isEmpty()) {
+            return 0;
+        } else {
+            int num = 0;
+            if (this.root == item) {
+                num++;
+            }
+            for(Tree tree: this.subtrees) {
+                num += tree.count(item);
+            }
+            return num;
+        }
+    }
+
+    public String toString() {
+        return this.toString(0);
+    }
+
+    public String toString(int depth) {
+        if(this.isEmpty()) {
+
+        }
+    }
+
 }
